d58c54e84962c2243d081399686afca96c5b9cec
Mohammed Alassi <mohammedaalassi@gmail.com>
2025-10-07 22:52:23 +0300

    hello back

--- Backend/Khayal/Khayal.Data/obj/Debug/net8.0/Khayal.Data.AssemblyInfo.cs
+++ Backend/Khayal/Khayal.Data/obj/Debug/net8.0/Khayal.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Khayal.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09dc8ecbe9dfa8cd2286504eaeffea74244fdafd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092c5874ab5c389454e60fc73d3f7ae5a532dc04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Khayal.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Khayal.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
