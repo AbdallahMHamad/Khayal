4a34056daf0f38e64704c603a580480f0bf730e1
Abdallah <Aboodghannam0@gmail.com>
2025-10-08 20:05:16 +0300

    i just made a change by making the features.map function commented thats the issue reason and i search about it and re do it

--- Frontend/khayal/src/components/Features.jsx
+++ Frontend/khayal/src/components/Features.jsx
@@ -9,12 +9,12 @@ export default function Features() {
   return (
     <section id="features" className="py-24 bg-black text-white">
       <div className="max-w-5xl mx-auto px-6 grid md:grid-cols-3 gap-8">
-        {features.map((f, i) => (
+        {/* {features.map((f, i) => (
           <div key={i} className="bg-gray-900 p-6 rounded-xl shadow-md">
             <h3 className="text-xl font-bold mb-2">{f.title}</h3>
             <p className="text-gray-300">{f.desc}</p>
           </div>
-        ))}
+        ))} */}
       </div>
     </section>
   );
